e1b63bb3d92f1f1877b3de9fd267352f81e65f48
pehlse <pedro97henrique@gmail.com>
2020-04-19 23:27:57 -0300

    first commit

---
+++ lib/page/home.dart
@@ -0,0 +1,143 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+      body: Container(
+        padding: EdgeInsets.all(25.0),
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+          begin: Alignment.topRight,
+          end: Alignment.bottomLeft,
+          colors: [
+            Color.fromRGBO(248, 248, 250, 1),
+            Color.fromRGBO(211, 232, 240, 1)
+          ],
+        )),
+        child: Column(
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Container(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        'Pedro',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 26,
+                          fontFamily: 'Verdana',
+                          fontStyle: FontStyle.normal,
+                          decoration: TextDecoration.none,
+                        ),
+                      ),
+                      Text(
+                        'Sunday, 21 April',
+                        style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 15,
+                          decoration: TextDecoration.none,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                Container(
+                  width: 50.0,
+                  height: 50.0,
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: NetworkImage(
+                            'https://i.pinimg.com/originals/26/78/9e/26789e89c810065bcc629b15b42ba6f4.jpg'),
+                      )),
+                )
+              ],
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 30.0),
+              decoration: BoxDecoration(
+                  color: Colors.white, borderRadius: BorderRadius.circular(10)),
+              child: TextField(
+                  decoration: InputDecoration(
+                hintText: 'Seach Courses',
+                border: InputBorder.none,
+                prefixIcon: Icon(Icons.search),
+                suffixIcon: Icon(Icons.filter_list),
+              )),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 30.0),
+              height: 180,
+              decoration: BoxDecoration(
+                color: Colors.purple[800],
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Padding(
+                padding: EdgeInsets.all(20),
+                child: Row(
+                  children: <Widget>[
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          'Daily',
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 25,
+                          ),
+                        ),
+                        Text(
+                          'Communication',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 25,
+                          ),
+                        ),
+                        Text(
+                          'Learnig Course',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 25,
+                          ),
+                        ),
+                        Spacer(),
+                        Container(
+                          height: 35,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(7),
+                            color: Color.fromRGBO(255, 107, 102, 1),
+                          ),
+                          child: FlatButton(
+                            child: Text(
+                              'Continue',
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
+                            ),
+                            onPressed: () {},
+                          ),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    ));
+  }
+}
